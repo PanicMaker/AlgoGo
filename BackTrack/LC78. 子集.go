a4fd44b99e72fc16299c60f1b92af53efacf508e
PanicMaker <mxc2012@outlook.com>
2024-08-06 00:19:27 +0800

    Update: LeetCode 78

--- BackTrack/LC78. 子集.go
+++ BackTrack/LC78. 子集.go
@@ -1,8 +1,12 @@
 package BackTrack
 
+import (
+	"slices"
+)
+
 // https://leetcode.cn/problems/subsets/
 
-func subsets(nums []int) [][]int {
+func subsetsI(nums []int) [][]int {
 	n := len(nums)
 
 	ans := make([][]int, 0)
@@ -10,15 +14,40 @@ func subsets(nums []int) [][]int {
 
 	var dfs func(i int)
 	dfs = func(i int) {
-		tmp := make([]int, len(path))
-		copy(tmp, path)
-		ans = append(ans, tmp)
+		ans = append(ans, slices.Clone(path))
 
 		for j := i; j < n; j++ {
 			path = append(path, nums[j])
 			dfs(j + 1)
 			path = path[:len(path)-1]
 		}
+	}
+
+	dfs(0)
+
+	return ans
+}
+
+func subsetsII(nums []int) [][]int {
+	n := len(nums)
+
+	ans := make([][]int, 0)
+	path := make([]int, 0)
+
+	var dfs func(i int)
+	dfs = func(i int) {
+		if i == n {
+			ans = append(ans, slices.Clone(path))
+			return
+		}
+
+		// 当前数字不选
+		dfs(i + 1)
+
+		// 选择当前数字
+		path = append(path, nums[i])
+		dfs(i + 1)
+		path = path[:len(path)-1]
 	}
 
 	dfs(0)
