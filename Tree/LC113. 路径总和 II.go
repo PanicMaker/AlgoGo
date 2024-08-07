a331bd29c5f183e6c72b108d82ded9a2fdebbf86
PanicMaker <mxc2012@outlook.com>
2024-08-07 23:13:10 +0800

    Add: LeetCode 113

---
+++ Tree/LC113. 路径总和 II.go
@@ -0,0 +1,31 @@
+package Tree
+
+// https://leetcode.cn/problems/path-sum-ii/description/
+
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	ans := make([][]int, 0)
+	path := make([]int, 0)
+
+	var backtrack func(node *TreeNode, target int)
+	backtrack = func(node *TreeNode, target int) {
+		if node == nil {
+			return
+		}
+
+		newTarget := target - node.Val
+		if node.Left == nil && node.Right == nil && newTarget == 0 {
+			tmp := make([]int, len(path))
+			copy(tmp, path)
+			ans = append(ans)
+		}
+
+		path = append(path, node.Val)
+		backtrack(node.Left, newTarget)
+		backtrack(node.Right, newTarget)
+		path = path[:len(path)-1]
+	}
+
+	backtrack(root, targetSum)
+
+	return ans
+}
