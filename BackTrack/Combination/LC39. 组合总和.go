a0e1ebd5a4a1f917b75b925e5ca3e957ec41e7e8
PanicMaker <mxc2012@outlook.com>
2024-08-12 15:42:24 +0800

    Update: LeetCode 39

--- BackTrack/Combination/LC39. 组合总和.go
+++ BackTrack/Combination/LC39. 组合总和.go
@@ -2,7 +2,7 @@ package Combination
 
 // https://leetcode.cn/problems/combination-sum/description
 
-func combinationSum(candidates []int, target int) [][]int {
+func combinationSumI(candidates []int, target int) [][]int {
 	n := len(candidates)
 	ans := make([][]int, 0)
 	path := make([]int, 0)
@@ -28,6 +28,42 @@ func combinationSum(candidates []int, target int) [][]int {
 			target += candidates[j]
 			path = path[:len(path)-1]
 		}
+	}
+
+	backtrack(0, target)
+
+	return ans
+}
+
+func combinationSumII(candidates []int, target int) [][]int {
+	n := len(candidates)
+	ans := make([][]int, 0)
+	path := make([]int, 0)
+
+	var backtrack func(i int, target int)
+	backtrack = func(i int, target int) {
+
+		if i == n || target < 0 {
+			return
+		}
+
+		if target == 0 {
+			tmp := make([]int, len(path))
+			copy(tmp, path)
+			ans = append(ans, tmp)
+			return
+		}
+
+		// 不选当前数
+		backtrack(i+1, target)
+
+		// 选择当前数
+		newTarget := target - candidates[i]
+		path = append(path, candidates[i])
+		backtrack(i, newTarget)
+		target += candidates[i]
+		path = path[:len(path)-1]
+
 	}
 
 	backtrack(0, target)
