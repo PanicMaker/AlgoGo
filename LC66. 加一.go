f5221e2cf1a65e049ff709306d139fa71c3a834b
PanicMaker <mxc2012@outlook.com>
2024-10-08 16:53:20 +0800

    Add: LeetCode 66

---
+++ LC66. 加一.go
@@ -0,0 +1,42 @@
+package algogo
+
+import (
+	"strconv"
+	"strings"
+)
+
+// https://leetcode.cn/problems/plus-one/description/
+
+// 会超出Int最大长度
+func plusOne1(digits []int) []int {
+	num := 0
+
+	for i := range digits {
+		num = num*10 + digits[i]
+	}
+
+	numStrs := strings.Split(string(num), "")
+	res := make([]int, 0)
+
+	for i := range numStrs {
+		num, _ := strconv.Atoi(numStrs[i])
+		res = append(res, num)
+	}
+
+	return res
+}
+
+func plusOne2(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		digits[i] += 1
+		digits[i] %= 10
+
+		if digits[i] != 0 {
+			return digits
+		}
+	}
+
+	newArr := make([]int, len(digits)+1)
+	newArr[0] = 1
+	return newArr
+}
