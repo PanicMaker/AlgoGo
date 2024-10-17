241d89e88278d304db72399c8e3208d21e1ce872
PanicMaker <mxc2012@outlook.com>
2024-10-17 17:52:38 +0800

    Update: LeetCode 35

--- BinarySearch/LC35. 搜索插入位置.go
+++ BinarySearch/LC35. 搜索插入位置.go
@@ -2,10 +2,28 @@ package binarysearch
 
 // https://leetcode.cn/problems/search-insert-position/description
 
-func searchInsert(nums []int, target int) int {
-	left, right := 0, len(nums)-1
+// 左闭又开
+func searchInsert1(nums []int, target int) int {
+	left, right := 0, len(nums)
 
 	for left < right {
+		mid := left + (right-left)/2
+
+		if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+
+	return left
+}
+
+// 闭区间
+func searchInsert2(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
 		mid := left + (right-left)/2
 
 		if nums[mid] < target {
