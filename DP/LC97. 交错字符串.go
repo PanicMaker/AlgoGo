3a8a121053de46c9275c07f5ed1d25a6767293ff
PanicMaker <mxc2012@outlook.com>
2024-11-01 18:32:15 +0800

    Update: LeetCode 97

--- DP/LC97. 交错字符串.go
+++ DP/LC97. 交错字符串.go
@@ -2,7 +2,7 @@ package DP
 
 // https://leetcode.cn/problems/interleaving-string
 
-func isInterleave(s1 string, s2 string, s3 string) bool {
+func isInterleave1(s1 string, s2 string, s3 string) bool {
 	m, n := len(s1), len(s2)
 	l := len(s3)
 
@@ -55,4 +55,42 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
 	}
 
 	return dfs(m-1, n-1)
+}
+
+func isInterleave2(s1 string, s2 string, s3 string) bool {
+	m, n := len(s1), len(s2)
+	l := len(s3)
+
+	// 如果 s1 和 s2 的总长度不等于 s3 的长度，直接返回 false
+	if m+n != l {
+		return false
+	}
+
+	// 初始化 dp 数组
+	dp := make([][]bool, m+1)
+	for i := range dp {
+		dp[i] = make([]bool, n+1)
+	}
+
+	// 初始化 dp 的初始状态
+	dp[0][0] = true
+
+	// 填充第一列
+	for i := 1; i <= m; i++ {
+		dp[i][0] = dp[i-1][0] && s1[i-1] == s3[i-1]
+	}
+
+	// 填充第一行
+	for j := 1; j <= n; j++ {
+		dp[0][j] = dp[0][j-1] && s2[j-1] == s3[j-1]
+	}
+
+	// 填充剩余的 dp 数组
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			dp[i+1][j+1] = (dp[i][j+1] && s1[i] == s3[i+j+1]) || (dp[i+1][j] && s2[j] == s3[i+j+1])
+		}
+	}
+
+	return dp[m][n]
 }
