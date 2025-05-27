ad50c270d7d274c09b6827d0bf82b758a0b6f341
PanicMaker <mxc2012@outlook.com>
2025-05-27 17:52:14 +0800

    Add: LeetCode 264

---
+++ DP/LC264. 丑数 II_test.go
@@ -0,0 +1,21 @@
+package DP
+
+import "testing"
+
+func Test_nthUglyNumber(t *testing.T) {
+	tests := []struct {
+		n    int
+		want int
+	}{
+		{1, 1},
+		{10, 12},
+		{15, 24},
+		{100, 1536},
+	}
+	for _, tt := range tests {
+		got := nthUglyNumber(tt.n)
+		if got != tt.want {
+			t.Errorf("nthUglyNumber(%d) = %d, want %d", tt.n, got, tt.want)
+		}
+	}
+}
