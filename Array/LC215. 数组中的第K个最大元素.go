68a588a02298dec3cf63a78b392d67e99e606a16
PanicMaker <mxc2012@outlook.com>
2025-07-16 23:16:17 +0800

    Add: LC215 实现数组中的第K个最大元素的查找算法

---
+++ Array/LC215. 数组中的第K个最大元素.go
@@ -0,0 +1,33 @@
+package Array
+
+// https://leetcode.cn/problems/kth-largest-element-in-an-array/
+
+func findKthLargest(nums []int, k int) int {
+	minVal, maxVal := nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		if maxVal < nums[i] {
+			maxVal = nums[i]
+		} else if minVal > nums[i] {
+			minVal = nums[i]
+		}
+	}
+
+	count := make([]int, maxVal-minVal+1)
+	for _, v := range nums {
+		count[v-minVal]++
+	}
+
+	// 累加计数数组
+	for i := 1; i < len(count); i++ {
+		count[i] += count[i-1]
+	}
+
+	res := make([]int, len(nums))
+	for i := len(nums) - 1; i >= 0; i-- {
+		v := nums[i]
+		res[count[v-minVal]-1] = v
+		count[v-minVal]--
+	}
+
+	return res[len(nums)-k]
+}
