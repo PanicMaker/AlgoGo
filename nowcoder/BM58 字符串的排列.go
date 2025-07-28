aab137cd796360e7ffca135b252b94e2ab890c13
PanicMaker <mxc2012@outlook.com>
2025-07-28 10:45:21 +0800

    Add: BM58 字符串的排列

---
+++ nowcoder/BM58 字符串的排列.go
@@ -0,0 +1,40 @@
+package nowcoder
+
+import "sort"
+
+func Permutation(str string) []string {
+	n := len(str)
+	if n == 0 {
+		return []string{}
+	}
+
+	ans := make([]string, 0)
+	path := make([]byte, 0)
+	used := make([]bool, n)
+	s := []byte(str)
+	sort.Slice(s, func(i, j int) bool { return s[i] < s[j] })
+
+	var dfs func()
+	dfs = func() {
+		if len(path) == n {
+			ans = append(ans, string(path))
+			return
+		}
+		for i := 0; i < n; i++ {
+			if used[i] {
+				continue
+			}
+			if i > 0 && s[i] == s[i-1] && !used[i-1] {
+				continue
+			}
+			used[i] = true
+			path = append(path, s[i])
+			dfs()
+			path = path[:len(path)-1]
+			used[i] = false
+		}
+	}
+
+	dfs()
+	return ans
+}
