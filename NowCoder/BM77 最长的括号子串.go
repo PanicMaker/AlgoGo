6fec346df568ab980dadb4c0cf92afad7e8ca0cc
PanicMaker <mxc2012@outlook.com>
2025-09-10 16:46:56 +0800

    Update: BM77 最长的括号子串

--- NowCoder/BM77 最长的括号子串.go
+++ NowCoder/BM77 最长的括号子串.go
@@ -2,7 +2,7 @@ package nowcoder
 
 // https://www.nowcoder.com/practice/45fd68024a4c4e97a8d6c45fc61dc6ad
 
-func longestValidParentheses(s string) int {
+func longestValidParentheses1(s string) int {
 	stack := make([]int, 0)
 
 	res := 0
@@ -30,4 +30,73 @@ func longestValidParentheses(s string) int {
 	}
 
 	return res
+}
+
+func longestValidParentheses2(s string) int {
+	n := len(s)
+	dp := make([]int, n+1)
+	maxLen := 0
+
+	for i := 1; i <= n; i++ {
+		if s[i-1] == ')' {
+			if i-2 >= 0 && s[i-2] == '(' {
+				dp[i] = dp[i-2] + 2
+			}
+			if i-2 >= 0 && s[i-2] == ')' {
+				prevIndex := i - 1 - dp[i-1] - 1
+				if prevIndex >= 0 && s[prevIndex] == '(' {
+					dp[i] = dp[i-1] + 2
+					if prevIndex > 0 {
+						dp[i] += dp[prevIndex]
+					}
+				}
+			}
+		}
+
+		maxLen = max(maxLen, dp[i])
+	}
+
+	return maxLen
+}
+
+func longestValidParentheses3(s string) int {
+	n := len(s)
+	maxLen := 0
+	left, right := 0, 0
+
+	for i := range s {
+		if s[i] == '(' {
+			left++
+		} else {
+			right++
+		}
+
+		if left == right {
+			maxLen = max(maxLen, 2*right)
+		}
+
+		if right > left {
+			left, right = 0, 0
+		}
+	}
+
+	left, right = 0, 0
+
+	for i := n - 1; i >= 0; i-- {
+		if s[i] == '(' {
+			left++
+		} else {
+			right++
+		}
+
+		if left == right {
+			maxLen = max(maxLen, 2*right)
+		}
+
+		if left > right {
+			left, right = 0, 0
+		}
+	}
+
+	return maxLen
 }
